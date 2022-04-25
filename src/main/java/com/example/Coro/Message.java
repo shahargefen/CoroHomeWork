92674bf210ad75c27ed10f65dea6a1676b652913
shahar <gefensha@post.bgu.ac.il>
2022-04-25 12:46:38 +0300

    message checker function

--- src/main/java/com/example/Coro/Message.java
+++ src/main/java/com/example/Coro/Message.java
@@ -1,27 +1,76 @@
 package com.example.Coro;
 
-import lombok.Data;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Data
 @Document
 public class Message {
+
+
     @Id
     private String id;
-    private Email sender;
-    private List<Email> recipients;
+    private String sender;
+    private List<String> recipients;
     private String subject;
     private String body;
     private long sendTime;
 
-    public Message(Email sender, List<Email> recipients, String subject, String body, long sendTime) {
+    public Message(String sender, List<String> recipients, String subject, String body, long sendTime) {
         this.sender = sender;
         this.recipients = recipients;
         this.subject = subject;
         this.body = body;
+        this.sendTime = sendTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public List<String> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<String> recipients) {
+        this.recipients = recipients;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(long sendTime) {
         this.sendTime = sendTime;
     }
 }
--- src/main/java/com/example/Coro/MessageService.java
+++ src/main/java/com/example/Coro/MessageService.java
@@ -3,6 +3,9 @@ package com.example.Coro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class MessageService {
 
@@ -19,9 +22,26 @@ public class MessageService {
         }
     }
 
-    public boolean CheckMessage(Message message){
-        return true;
+    public boolean CheckMessage(Message message) {
+        String body = message.getBody().toLowerCase();
+        String subject = message.getSubject().toLowerCase();
+        String ccPattern = "(?<!\\d)\\d{16}(?!\\d)|(?<!\\d)\\d{4}[-]\\d{4}[-]\\d{4}[-]\\d{4}(?!\\d)|(?<!\\d)\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]\\d{4}(?!\\d)";
+        Pattern pattern = Pattern.compile(ccPattern);
+        Matcher matcherBody = pattern.matcher(body);
+        Matcher matcherSubject = pattern.matcher(subject);
+
+        if (subject.contains("creditcard") || subject.contains("credit card") || matcherSubject.find()){
+            return true;
+        }
+        if (body.contains("creditcard") || body.contains("credit card") || matcherBody.find()) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+
+
 
 
 }
